90fb29690a82a954f84fba84e83df2fc5e250ed0
efirestone <efirestone@users.noreply.github.com>
2019-01-03 02:44:42 -0600

    Allow config file mapping
    Allows external config files to be mapped in without interfering with the monitor script itself (which is also in the monitor directory). Config files should be mapped into `/config`

--- startup.sh
+++ startup.sh
@@ -8,11 +8,16 @@ if [ ! -e $FIRST_RUN_CHECK ]; then
     git clone git://github.com/andrewjfreyer/monitor
     chmod +x /monitor/monitor.sh
     touch $FIRST_RUN_CHECK
-    echo "-- Starting Monitor --"
-    cd monitor
-    ( echo n ) | ./monitor.sh $MON_OPT
-else
-    echo "-- Starting Monitor --"
-    cd monitor
-    ( echo n ) | ./monitor.sh $MON_OPT
 fi
+
+# Use volume-mounted config files if present.
+# Use -v /your/config/dir:/config to map in files like known_static_addresses
+cp /config/address_blacklist /monitor/address_blacklist 2> /dev/null
+cp /config/behavior_preferences /monitor/behavior_preferences 2> /dev/null
+cp /config/known_beacon_addresses /monitor/known_beacon_addresses 2> /dev/null
+cp /config/known_static_addresses /monitor/known_static_addresses 2> /dev/null
+cp /config/mqtt_preferences /monitor/mqtt_preferences 2> /dev/null
+
+echo "-- Starting Monitor --"
+cd monitor
+( echo n ) | ./monitor.sh $MON_OPT
